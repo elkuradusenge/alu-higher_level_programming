46a086175196dc2858a2281e0e063ca4e601e501
elkuradusenge <e.kuradusen@alustudent.com>
2025-06-27 21:04:03 +0200

    Update 0-positive_or_negative.py

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-def positive_or_negative(number):
-    if not isinstance(number, int):
-        raise TypeError("number must be an integer")
-    if number > 0:
-        print(f"{number:d} is positive")
-    elif number == 0:
-        print(f"{number:d} is zero")
-    else:
-        print(f"{number:d} is negative")
+import random
+number = random.randint(-10, 10)
 
-
-if __name__ == "__main__":
-    import random
-    number = random.randint(-10000, 10000)
-    positive_or_negative(number)
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
+    
