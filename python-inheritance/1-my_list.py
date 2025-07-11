b18a47244da76d512d8c007c2db835fa0d3f7390
elkuradusenge <e.kuradusen@alustudent.com>
2025-07-11 22:05:26 +0200

    inhertance

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===========================
+Module
+===========================
+"""
+
+
+class MyList(list):
+    """method print_sorted"""
+    pass
+
+    def print_sorted(self):
+        """this sorted a list"""
+
+        print(sorted(list(self)))
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" declaring  class geometry"""
+
+
+class BaseGeometry:
+    """
+    A class representing base geometry.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message "
+        """
+        raise Exception("area() is not implemented")
