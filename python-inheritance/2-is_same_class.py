d8bd38d215594f71946cbe915fa73ed97d08ad0d
elkuradusenge <e.kuradusen@alustudent.com>
2025-07-11 22:05:26 +0200

    inhertance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module that creates a class"""
+
+
+def is_same_class(obj, a_class):
+    """class """
+    return type(obj) is a_class
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define an empty class"""
+
+
+class BaseGeometry:
+    """
+    An empty class which represents base geometry.
+    """
+    pass
